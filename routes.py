6715e8a88401b77b71c5b54b78cb7572740666b5
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2026-02-28 16:14:22 -0500

    added login to nav

--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 from flask import render_template
 
 def setup_routes(app):
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
+    
     @app.route("/")
     def index():
         return render_template("index.html")
@@ -8,3 +12,7 @@ def setup_routes(app):
     @app.route("/gaming")
     def gaming():
         return render_template("gaming.html")
+    
+    @app.route("/profile")
+    def profile():
+        return render_template("profile.html")
