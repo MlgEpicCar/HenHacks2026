779f5424eb4db6366e624e9797e1f8eddac15a69
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2026-02-28 12:22:26 -0500

    navbar fix

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, redirect
 from models import db, User, Goal
+from models import db
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()  # Creates all tables defined in models.py
 
 @app.route("/")
 def index():
